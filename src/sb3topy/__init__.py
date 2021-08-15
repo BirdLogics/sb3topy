73c38b914fceae7661c6fdc0a369d54af9dc106b
jahunsbe1 <47367734+jahunsbe1@users.noreply.github.com>
2021-08-14 22:32:16 -0400

    Removed deprecated function

--- src/sb3topy/__init__.py
+++ src/sb3topy/__init__.py
@@ -6,10 +6,3 @@ __init__.py
 
 from . import config, packer, parser, project, unpacker
 from .parser import sanitizer
-
-
-def load_config(path):
-    """Reloads config with a given json path"""
-    import importlib
-    config.CONFIG_PATH = path
-    importlib.reload(config)
