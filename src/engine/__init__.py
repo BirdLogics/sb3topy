9923b664b6ca6b1845acc986aca276f59ebabd05
jahunsbe1 <47367734+jahunsbe1@users.noreply.github.com>
2021-09-28 17:17:24 -0400

    Additional imports

--- src/engine/__init__.py
+++ src/engine/__init__.py
@@ -7,6 +7,6 @@ Contains everything needed to run a converted project.
 # flake8: noqa
 
 
-from . import config
+from . import config, events, operators
 from .runtime import start_program
 from .types import costumes, lists, pen, sounds, target
